851e5bb1157e5cf3440a8fad7ece4ee66650c313
organicbran <bryanchung987@gmail.com>
2024-12-20 19:11:56 -0800

    Reset

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OutlineEffect } from 'three/addons/effects/OutlineEffect.js';
 import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
+import { OutlineEffect } from 'three/addons/effects/OutlineEffect.js';
 
 const scene = new THREE.Scene();
-
 const container = document.getElementById("hero");
 const camera = new THREE.PerspectiveCamera(15, container.clientWidth / container.clientHeight, 1, 1000);
 
@@ -16,50 +15,21 @@ renderer.toneMapping = THREE.NoToneMapping;
 renderer.toneMappingExposure = 1;
 container.appendChild(renderer.domElement);
 
-const effect = new OutlineEffect(renderer, {
-    defaultThickness: 0,
-    defaultColor: [ 0, 0, 0 ],
-    defaultAlpha: 1,
-    defaultKeepAlive: true
-});
+// const effect = new OutlineEffect(renderer, {
+//     defaultThickness: 0,
+//     defaultColor: [ 0, 0, 0 ],
+//     defaultAlpha: 1,
+//     defaultKeepAlive: true
+// });
 
-const light = new THREE.DirectionalLight(0xffffff, 2);
-light.position.set(1, 1, 2);
-//scene.add(light);
+const environment = new RoomEnvironment();
+const pmremGenerator = new THREE.PMREMGenerator( renderer );
 
-const fill = new THREE.AmbientLight(0xffffff, 1.25);
-//scene.add(fill);
+scene.environment = pmremGenerator.fromScene( environment ).texture;
+scene.environmentIntensity = 0.8;
 
-const textureLoader = new THREE.TextureLoader();
-const cubeTextureLoader = new THREE.CubeTextureLoader()
-const environmentMap = cubeTextureLoader.load([
-    '/assets/env/px.png',
-    '/assets/env/nx.png',
-    '/assets/env/py.png',
-    '/assets/env/ny.png',
-    '/assets/env/pz.png',
-    '/assets/env/nz.png'
-]);
-scene.environment = environmentMap;
-scene.environmentRotation = new THREE.Euler(Math.PI, Math.PI * 1.5);
-scene.environmentIntensity = 2;
-
-// const environment = new RoomEnvironment();
-// const pmremGenerator = new THREE.PMREMGenerator( renderer );
-
-// scene.environment = pmremGenerator.fromScene( environment ).texture;
-// scene.environment = pmremGenerator.fromEquirectangular( "assets/studio.hdr" );
-// scene.environmentRotation = new THREE.Euler(0, Math.PI * 0.1, 0);
-// scene.environmentIntensity = 1;
-
-const geometry = new THREE.BoxGeometry(1, 1, 0.25);
-//const geometry = new THREE.SphereGeometry(0.5, 24, 12);
-//const aoTexture = textureLoader.load("/assets/ao.png")
-const material = new THREE.MeshPhysicalMaterial({ color: 0x7a87ff, roughness: 1, metalness: 0 });
-//material.aoMap = aoTexture;
-//const material = new THREE.MeshPhysicalMaterial({ color: 0xffffff, roughness: 0.2, wireframe: true });
-//const model = new THREE.Mesh(geometry, material);
-//scene.add(model);
+// const material = new THREE.MeshPhysicalMaterial({ color: 0x627fff, roughness: 1, metalness: 0 });
+const material = new THREE.MeshPhysicalMaterial({ color: 0xffffff, roughness: 1, wireframe: true });
 
 let model;
 const loader = new GLTFLoader();
@@ -67,8 +37,7 @@ loader.load( '/assets/handheld.glb', function (gltf) {
     scene.add(gltf.scene);
     model = gltf.scene.children[0];
     model.material.aoMapIntensity = 2;
-    model.material.roughness = 1;
-    model.material.metalness = 0;
+    // model.material = material;
 });
 
 camera.position.z = 10;
@@ -80,18 +49,21 @@ window.addEventListener('pointermove', function () {
     mouseY = (event.clientY / window.innerHeight) * -2 + 1;
 });
 
-const rotationSmooth = 0.1;
-const rotationAngle = 0.5;
+const rotationSmooth = 8;
+const rotationAngle = Math.PI / 4;
 
+const clock = new THREE.Clock();
 function animate() {
+    let delta = clock.getDelta();
     if (model != null) {
-        model.rotation.y = THREE.MathUtils.lerp(model.rotation.y, mouseX * rotationAngle, rotationSmooth);
-        model.rotation.x = THREE.MathUtils.lerp(model.rotation.x, mouseY * -rotationAngle, rotationSmooth);
+        model.rotation.y = THREE.MathUtils.lerp(model.rotation.y, mouseX * rotationAngle, rotationSmooth * delta);
+        model.rotation.x = THREE.MathUtils.lerp(model.rotation.x, mouseY * -rotationAngle, rotationSmooth * delta);
     }
-    effect.render(scene, camera);
+    renderer.render(scene, camera);
+    // effect.render(scene, camera);
 }
 
-window.onscroll = function() {scroll()};
+window.onscroll = function() { scroll() };
 function scroll() {
     let switchPoint = window.scrollY + document.getElementById("panel-2").getBoundingClientRect().top - document.getElementById("bar-container").clientHeight;
     if (document.body.scrollTop > switchPoint || document.documentElement.scrollTop > switchPoint) {
@@ -101,20 +73,12 @@ function scroll() {
         for (var i = 0; i < barlinks.length; i++) {
             barlinks[i].classList.add("bar-link-light");
         }
-        var barlinkbuttons = document.getElementsByClassName("bar-link-button");
-        for (var i = 0; i < barlinkbuttons.length; i++) {
-            barlinkbuttons[i].classList.add("bar-link-button-light");
-        }
     } else {
         document.body.style.backgroundColor = getComputedStyle(document.body).getPropertyValue('--blurple');
         document.getElementById("bar-container").classList.remove("light")
         var barlinks = document.getElementsByClassName("bar-link");
         for (var i = 0; i < barlinks.length; i++) {
             barlinks[i].classList.remove("bar-link-light");
-        }
-        var barlinkbuttons = document.getElementsByClassName("bar-link-button");
-        for (var i = 0; i < barlinkbuttons.length; i++) {
-            barlinkbuttons[i].classList.remove("bar-link-button-light");
         }
     }
 }
